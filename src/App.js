a439a2e6ee6ba708c74de7ac52bd72ee8aa6c681
Weijie XI <wxi76@berkeley.edu>
2024-08-09 12:15:44 -0700

    added theme-overrides

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
-import "../styles/index.less";
+import React from 'react';
+
+import "../styles/App.less";
 
 export function App() {
-    return <h1>Hello world!</h1>;
+    return (
+        <div className="landing-main-text">
+            <h1>Welcome to Labs Basic SPA</h1>
+            <div className="landing-main-text">
+                <p>
+                    This is <span>an example</span> of how we&apos;d like for you to approach page/routable
+                    components.
+                </p>
+
+                {/*<AntButton type="primary">Primary Button</AntButton>*/}
+
+            </div>
+        </div>
+    );
   }
